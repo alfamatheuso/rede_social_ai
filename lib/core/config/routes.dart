2f12f7ab308e6a4461ef09e76eda9fefa54d13e0
Matheus Oliveira <matheuso@alfaconsultorias.com.br>
2025-04-22 10:37:15 -0300

    Se ainda faltar alguma tela adicione

--- lib/core/config/routes.dart
+++ lib/core/config/routes.dart
@@ -2,5 +2,10 @@ final routes = {
   '/login': (context) => LoginPage(controller: LoginController(LoginRepository())),
   '/register': (context) => RegisterPage(controller: RegisterController(RegisterRepository())),
   '/home': (context) => HomePage(controller: HomeController(HomeRepository())),
-  '/profile': (context) => ProfilePage(controller: ProfileController(ProfileRepository())),
+  '/profile': (context) {
+    final args = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>? ?? {};
+    final userId = args['userId'] as String? ?? '1';
+    return ProfilePage(controller: ProfileController(ProfileRepository()), userId: userId);
+  },
+  '/create_post': (context) => CreatePostPage(controller: CreatePostController(CreatePostRepository())),
 };
